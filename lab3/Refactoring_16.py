c2e33cd9a7635eacbc9e59365b92c45474801b86
Smeshutka <m.s.sivak@mail.ru>
2021-10-11 19:02:08 +0300

    Make refactoring of picture

--- lab3/Refactoring_16.py
+++ lab3/Refactoring_16.py
@@ -11,8 +11,8 @@ LIGHTCYAN = (224,255,255)
 SKYBLUE = (0,204,255)
 
 FPS = 30
-screen = pygame.display.set_mode((800, 800))
-screen.fill((51, 51, 51))
+screen = pygame.display.set_mode((800, 1000))
+screen.fill((81, 81, 81))
 
 def clouds(surface, x, y, w, h):
     '''
@@ -34,30 +34,16 @@ def clouds(surface, x, y, w, h):
     ellipse(sub, (150, 150, 150, 128), (640, 30, 120, 40))
     surface.blit(tr.scale(sub,(w,h)),(x,y))
     
-def flash(surface, x, y, w, h):
+def betterflash(surface, x, y, w, h):
     '''
     Функция рисует засвеченные облака на данном surface.
     surface - объект pygame.Surface
     x, y - координаты левого верхнего угла изображения
-    w, h - ширина и высота изобажения. Нормальные пропорции(заложенные при рисовании) если w=800 
+    w, h - ширина и высота изобажения. Нормальные пропорции(заложенные при рисовании) если w=3000, h=300
     '''
-    sub = pygame.Surface((800,800),pygame.SRCALPHA)
-    i = 0
-    m = 51
-    n = 51
-    k = 51
-    r = 300
-    l = -500
-    z = 500
-    while i < 25:
-        m += 6
-        n += 6
-        k += 6
-        r -= 10
-        l += 5
-        z += 5
-        i += 1
-        ellipse(sub, (m, n, k, 128), (l, z, 10*r, r))
+    sub = pygame.Surface((3000,300),pygame.SRCALPHA)
+    for i in range(25):
+        ellipse(sub, (255,255,255,5*i), (5*i,5*i,10*(300-10*i),300-10*i))
     surface.blit(tr.scale(sub,(w,h)),(x,y))
 
 def car(surface, x, y, w, h, inverse, color):
@@ -78,39 +64,44 @@ def car(surface, x, y, w, h, inverse, color):
     ellipse(sub, BLACK, (1700,650,400,300))
     surface.blit(tr.scale(tr.flip(sub,inverse,False),(w,h)),(x,y))
 
-def city(surface, x, y, w, h):
+def bettercity(surface, x, y, w, h, inverse, alpha):
     '''
     Функция рисует здания на данном surface.
     surface - объект pygame.Surface
     x, y - координаты левого верхнего угла изображения
-    w, h - ширина и высота изобажения. Нормальные пропорции(заложенные при рисовании) если w=800 h=520
+    w, h - ширина и высота изобажения. Нормальные пропорции(заложенные при рисовании) если w=606 h=643
+    inverse - булева переменная, если True, то изображение инвертируется по x
+    alpha - коэффициент прозрачности в типе pygame.Color
     '''
-    sub = pygame.Surface((800,520),pygame.SRCALPHA)
-    rect(sub, (50, 50, 50), (0, 0, 800, 120))
-    rect(sub, (1, 1, 1, 158), (500, 0, 100, 100))
-    rect(sub, (1, 1, 1, 58), (200, 0, 100, 100))
-    rect(sub, (1, 1, 1, 150), (0, 0, 100, 100))
-    rect(sub, (204, 204, 204), (500, 20, 150, 500))
-    rect(sub, (102, 102, 102), (310, 70, 120, 400))
-    rect(sub, (102, 102, 102), (450, 110, 120, 400))
-    rect(sub, (153, 204, 204), (10, 100, 120, 400))  
-    rect(sub, (153, 204, 204), (670, 90, 120, 400)) 
-    rect(sub, (220, 220, 220, 128), (20, 150, 300, 300))
-    rect(sub, (220, 220, 220, 128), (500, 120, 300, 300))
+    sub = pygame.Surface((606,643),pygame.SRCALPHA)
+    rect(sub, (255,255,255,alpha), (0,0,606,526))
+    rect(sub, (184,197,201,alpha), (3,3,600,520))
+    rect(sub, (148,169,174,alpha), (13,18,120,553))
+    rect(sub, (148,174,169,alpha), (150,40,130,510))
+    rect(sub, (184,201,197,alpha), (100,125,120,500))
+    
+    rect(sub, (220,228,227,alpha), (450,20,130,540))
+    rect(sub, (111,146,139,alpha), (400,150,130,500))
+    
+    
+    surface.blit(tr.scale(tr.flip(sub,inverse,False),(w,h)),(x,y))
 
-    surface.blit(tr.scale(sub,(w,h)),(x,y))
-
-
-car(screen, 270, 600, 230, 95, False, SKYBLUE)
-car(screen, 490, 660, 345, 142, True, SKYBLUE)
-car(screen, 530, 570, 161, 66, False, SKYBLUE)
-car(screen, 430, 520, 92, 38, True, SKYBLUE)
-car(screen, 200, 530, 138, 57, True, SKYBLUE)
-car(screen, 80, 600, 161, 66, False, SKYBLUE)
 
 clouds(screen, 0,0, 800,250)
-city(screen, 0,0, 800,520)
-flash(screen, 0,0, 800,800)
+
+bettercity(screen, 400,-100, 606,643, 0, 30)
+bettercity(screen, -100,-100, 606,643, 0, 30)
+bettercity(screen, 300,100, 606,643, 1, 255)
+bettercity(screen, -100,150, 606,643, 0, 255)
+
+car(screen, 270, 800, 230, 95, False, SKYBLUE)
+car(screen, 490, 860, 345, 142, True, SKYBLUE)
+car(screen, 530, 770, 161, 66, False, SKYBLUE)
+car(screen, 470, 720, 92, 38, True, SKYBLUE)
+car(screen, 200, 760, 138, 57, True, SKYBLUE)
+car(screen, 80, 800, 161, 66, False, SKYBLUE)
+
+betterflash(screen, -500,700, 3000,300)
 
 pygame.display.update()
 clock = pygame.time.Clock()
